b974f2b3aa5a64aab2e39e9351d1f00538eeb52f
murtazaisback <muhammadmurtazanasir90@gmail.com>
2024-01-06 12:34:40 +0300

    second

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -128,6 +128,9 @@ export default function Home() {
                 <p>Use our built-in designer to <br /> customize your seating chart and  <br /> allow customers to select their<br />seats at checkout</p>
               </div>
             </motion.div>
+            <div>
+              
+            </div>
           </div>
         </div>
         <motion.div
